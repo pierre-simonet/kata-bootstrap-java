a0126f353af51be8a0fabb4c23c9ac11d004db3b
Pierre Simonet <pierre.simonet@movesol.com>
2025-12-13 14:05:43 +0100

    fix: create proper skeloton class and test class

--- src/main/java/com/tdd/kata/Main.java
+++ src/main/java/com/tdd/kata/Main.java
@@ -1,6 +1,7 @@
+package com.tdd.kata;
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         System.out.println("Hello world");
     }
 
---
+++ src/test/java/com/tdd/kata/MainTest.java
@@ -0,0 +1,11 @@
+package com.tdd.kata;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    void sampleTest() {
+    }
+    
+}
